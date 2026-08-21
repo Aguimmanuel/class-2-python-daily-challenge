b7a74654912665b49918a0a99be32bb8de81b865
okwedavid <okwe7788@gmail.com>
2026-08-21 11:29:02 +0100

    David's solution

--- David/day_1_market_bill.py
+++ David/day_1_market_bill.py
@@ -1,19 +1,17 @@
 
-customer = print("\nWelcome!, ")
-
 Price_of_rice_per_kg = int(input("\nRice price for today: "))
 Price_of_beans_per_kg = int(input("\nBeans price for today: "))
 Price_of_garri_per_kg = int(input("\nGarri price for today: "))
 
 customer_choice_1 = float(input("\nHow many Kg of Rice? "))
-
 print(f"Today's Rice price is {Price_of_rice_per_kg} per Kg.")
+
 customer_choice_2= float(input("\nHow many Kg of Beans? "))
-print(f"Today's Rice price is {Price_of_beans_per_kg} per Kg.")
+print(f"Today's Beans' price is {Price_of_beans_per_kg} per Kg.")
 
 customer_choice_3= float(input("\nHow many Kg of Garri? "))
 
-print(f"Today's Rice price is {Price_of_garri_per_kg} per Kg.")
+print(f"Today's Garri price is {Price_of_garri_per_kg} per Kg.")
 
 rice_total = customer_choice_1 * Price_of_rice_per_kg
 beans_total = customer_choice_2 * Price_of_beans_per_kg
