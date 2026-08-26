129a48e9ace291a4ee62bee40d6e652117dd0650
favour Awo <favourawo@gmail.com>
2026-08-26 12:26:35 +0100

    day two

---
+++ fawo/day_three.py
@@ -0,0 +1,25 @@
+student_name = input("Student name: ")
+score = int(input("Score: "))
+
+if score > 100 or score < 0:
+    print("Enter a valid score.")
+if score >= 70 and score <= 100:
+    print(f"{student_name} scored {score}: A (Excellent)")
+    remaining = 100 - score
+    print(f"{remaining} more marks to reach A")
+elif score >= 60 and score <= 69:
+    print(f"{student_name} scored {score}: B (Very good)")
+    remaining = 70 - score
+    print(f"{remaining} more marks to reach A")
+elif score >= 50 and score <= 59:
+    print(f"{student_name} scored {score}: c (Good)")
+    remaining = 60 - score 
+    print(f"{remaining} more marks to reach B") 
+elif score >= 45 and score <= 49:
+    print(f"{student_name} scored {score}: D (pass)")
+    remaining = 50 - score
+    print(f"{remaining} more marks to reach C")
+else:
+    print(f"{student_name} scored {score}: F (Fail)")
+    remaining = 45 - score
+    print(f"{remaining} more marks to reach D")
