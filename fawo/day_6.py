5caaf3e32640e218ab2881b0e55a4b09c2f16258
favour Awo <favourawo@gmail.com>
2026-09-02 17:29:39 +0100

    day 7

--- fawo/day_6.py
+++ fawo/day_6.py
@@ -3,7 +3,9 @@ loyalty_member = input("Loyalty member (yes/no): ").lower()
 promo_day = input("Promo day (yes/no): ").lower()
 
 discount_approved = False
-if basket_total < 20000:
+if basket_total <= 0:
+    delivery_fee = 0
+elif basket_total < 20000:
     delivery_fee = 2500
 else:
     delivery_fee = 0
@@ -11,17 +13,17 @@ else:
 if loyalty_member == "yes" and basket_total > 0:
     discount_percent = 10
     discount_approved = True
-    discount_amount = basket_total - (basket_total * (discount_percent / 100))
+    discount_amount = (basket_total * (discount_percent / 100))
     final_total = basket_total - (basket_total * (discount_percent / 100)) + delivery_fee
     if promo_day == "yes":
         discount_percent = 15
-        discount_amount = basket_total - (basket_total * (discount_percent / 100))
-        final_total = basket_total - (basket_total * (discount_percent / 100)) + delivery_fee
+        discount_amount = (basket_total * (discount_percent / 100))
+        final_total = basket_total - (discount_amount + delivery_fee)
 
 elif loyalty_member == "no" and basket_total >= 50000:
     discount_percent = 5
     discount_approved = True
-    discount_amount = basket_total - (basket_total * (discount_percent / 100))
+    discount_amount = (basket_total * (discount_percent / 100))
     final_total = basket_total - (basket_total * (discount_percent / 100)) + delivery_fee
 elif loyalty_member == "no" and promo_day == "no":
     discount_percent = 0
@@ -32,7 +34,11 @@ elif loyalty_member == "no" and promo_day == "no":
         discount_percent = 10
         discount_amount = basket_total - (basket_total * (discount_percent / 100))
         final_total = basket_total - (basket_total * (discount_percent / 100)) + delivery_fee
-    
+else:
+    discount_percent = 0
+    discount_approved = False
+    discount_amount = basket_total * (discount_percent / 100)
+    final_total = basket_total - (basket_total * (discount_percent / 100)) + delivery_fee   
 
 print(f"DISCOUNT: {discount_percent}%")
 print(f"DISCOUNT AMOUNT: {discount_amount:.2f}")
