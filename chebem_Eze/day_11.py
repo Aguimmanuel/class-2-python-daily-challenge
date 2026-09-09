fce0b1e9461a70eeae8c184c14a8a51f98fd9f05
Chebemeze <Chebemeze@gmail.com>
2026-09-09 16:29:22 +0100

    Refactor Cafeteria Order Kiosk to correctly calculate total and display item additions

--- chebem_Eze/day_11.py
+++ chebem_Eze/day_11.py
@@ -7,17 +7,23 @@ print("2. Beans porridge - N900")
 print("3. Chilled drink - N400")
 print("4. Done")
 
-items_count = 0
+items_count, total, jollof_price, beans_price, drink_price = 0, 0, 1200, 900, 400
 while True:
     options = int(input("Choose (1-4): "))
     if options == 1:
         items_count+=1
+        total+=jollof_price
+        print("Jollof rice added - N1200.00")
         continue
     elif options == 2:
         items_count+=1
+        print("Beans porridge added - N900")
+        total+=beans_price
         continue
     elif options == 3:
         items_count+=1
+        print("Chilled drink added - N400.00")
+        total+=drink_price
         continue
     elif options == 4:
         break
@@ -25,4 +31,5 @@ while True:
         print("No such option")
         continue
 
-    
+print(f"\nITEMS: {items_count}")
+print(f"TOTAL: N{total:.2f}") 
